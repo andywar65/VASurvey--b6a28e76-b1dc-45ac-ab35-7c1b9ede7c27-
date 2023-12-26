d4c08b151b2ad1a862104c2d6f752c86be78d945
andywar65 <andy.war1965@gmail.com>
2023-12-26 20:17:34 +0100

    get wall objects and print their weight

--- dev/WriteBOQ_cmd.py
+++ dev/WriteBOQ_cmd.py
@@ -1,4 +1,8 @@
 import csv
+import clr
+clr.AddReference("VisualARQ.Script")
+import VisualARQ.Script as va
+import Rhino
 import rhinoscriptsyntax as rs
 
 __commandname__ = "WriteBOQ"
@@ -6,24 +10,31 @@ __commandname__ = "WriteBOQ"
 # RunCommand is the called when the user enters the command name in Rhino.
 # The command name is defined by the filname minus "_cmd.py"
 def RunCommand( is_interactive ):
-    #prompt the user for a file to import
+    #prompt the user for the Wall csv to import
     filter = "CSV file (*.csv)|*.csv|*.txt|All Files (*.*)|*.*||"
     filename = rs.OpenFileName("Open Wall Table File", filter)
     if not filename: return
- 
+    # extract a wall list with clean values
     with open(filename) as csvfile:
-        wall_list = []
+        wall_dict = {}
         original = csv.DictReader(csvfile)
         for row in original:
-            wall_list.append({
-                "ID": row["\xef\xbb\xbfDescription"],
+            wall_dict[row["\xef\xbb\xbfDescription"]]={
                 "Style": row["Style"],
                 "Length": row["Length"],
                 "Area": float(row["Area"].split(" ")[0]),
                 "Volume": float(row["Volume"].split(" ")[0]),
-            })
+            }
     csvfile.close()
-    print(wall_list)
+    # get all objects in file
+    objects = Rhino.RhinoDoc.ActiveDoc.Objects
+    for obj in objects:
+        if obj.IsNormal and va.IsWall(obj.Id):
+            volume = wall_dict[str(obj.Id)]["Volume"]
+            unit_w_id = va.GetObjectParameterId("Unit weight", obj.Id,1)
+            unit_w = va.GetParameterValue(unit_w_id, obj.Id)
+            if unit_w:
+                print(float(unit_w)*volume)
     return 0
   
 if __name__ == "__main__":
