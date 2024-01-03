665e1c011561568a8048fcdea2a2b21e0511b958
andywar65 <andy.war1965@gmail.com>
2024-01-03 23:47:29 +0100

    update construction work description

--- dev/AddConstructionParameters_cmd.py
+++ dev/AddConstructionParameters_cmd.py
@@ -8,25 +8,25 @@ physical_parameters = [
     ["Unit weight", va.ParameterType.Number, "Physical", "Unit weight of wall (kg/mc)"],
 ]
 wall_demolition_parameters = [
-    ["010-Demolizione", va.ParameterType.Boolean, "Demolizioni", "Demolizione totale del muro, al mq", True],
-    ["020-Spicconatura", va.ParameterType.Percentage, "Demolizioni", "Spicconatura dell'intonaco al mq (100% = una faccia)", False],
-    ["030-Raschiatura", va.ParameterType.Percentage, "Demolizioni", "Raschiatura della tinteggiatura al mq (100% = una faccia)", False],
-    ["040-Rimozione rivestimento", va.ParameterType.Percentage, "Demolizioni", "Rimozione del rivestimento al mq (100% = una faccia)", False],
+    ["010-Demolizione", va.ParameterType.Boolean, "Demolizioni", "Demolizione totale muraria", True],
+    ["020-Spicconatura", va.ParameterType.Percentage, "Demolizioni", "Spicconatura dell'intonaco murario", False],
+    ["030-Raschiatura", va.ParameterType.Percentage, "Demolizioni", "Raschiatura della tinteggiatura muraria", False],
+    ["040-Rimozione rivestimento", va.ParameterType.Percentage, "Demolizioni", "Rimozione del rivestimento murario", False],
 ]
 transportation_parameters = [
-    ["010-Scarriolatura", va.ParameterType.Boolean, "Trasporti", "Scarriolatura del muro demolito, al kg"],
-    ["020-Tiro", va.ParameterType.Boolean, "Trasporti", "Tiro in alto o calo in basso del muro demolito, al kg"],
-    ["030-Trasporto a discarica", va.ParameterType.Boolean, "Trasporti", "Carico e trasporto a discarica del muro demolito, al kg"],
-    ["040-Oneri di discarica", va.ParameterType.Boolean, "Trasporti", "Smaltimento in discarica del muro demolito, al kg"],
+    ["010-Scarriolatura", va.ParameterType.Boolean, "Trasporti", "Scarriolatura dei materiali demoliti"],
+    ["020-Tiro", va.ParameterType.Boolean, "Trasporti", "Tiro in alto o calo in basso dei materiali demoliti"],
+    ["030-Trasporto a discarica", va.ParameterType.Boolean, "Trasporti", "Carico e trasporto a discarica dei materiali demoliti"],
+    ["040-Oneri di discarica", va.ParameterType.Boolean, "Trasporti", "Smaltimento in discarica dei materiali demoliti"],
 ]
 wall_construction_parameters = [
-    ["010-Ricostruzione", va.ParameterType.Boolean, "Ricostruzioni", "Costruzione da zero del muro, al mq", True],
-    ["020-Intonacatura", va.ParameterType.Percentage, "Ricostruzioni", "Intonacatura del muro al mq (100% = una faccia)", False],
-    ["030-Rasatura", va.ParameterType.Percentage, "Ricostruzioni", "Rasatura del muro al mq (100% = una faccia)", False],
-    ["040-Imprimitura", va.ParameterType.Percentage, "Ricostruzioni", "Imprimitura del muro al mq (100%=una faccia)", False],
-    ["050-Tinteggiatura", va.ParameterType.Percentage, "Ricostruzioni", "Tinteggiatura del muro al mq (100%=una faccia)", False],
-    ["060-Verniciatura", va.ParameterType.Percentage, "Ricostruzioni", "Verniciatura a smalto del muro al mq (100%=una faccia)", False],
-    ["070-Rivestimento", va.ParameterType.Percentage, "Ricostruzioni", "Rivestimento del muro al mq (100%=una faccia)", False],
+    ["010-Ricostruzione", va.ParameterType.Boolean, "Ricostruzioni", "Costruzioni murarie", True],
+    ["020-Intonacatura", va.ParameterType.Percentage, "Ricostruzioni", "Intonacatura muraria", False],
+    ["030-Rasatura", va.ParameterType.Percentage, "Ricostruzioni", "Rasatura muraria", False],
+    ["040-Imprimitura", va.ParameterType.Percentage, "Ricostruzioni", "Imprimitura muraria con aggrappante", False],
+    ["050-Tinteggiatura", va.ParameterType.Percentage, "Ricostruzioni", "Tinteggiatura muraria con lavabile", False],
+    ["060-Verniciatura", va.ParameterType.Percentage, "Ricostruzioni", "Verniciatura muraria a smalto", False],
+    ["070-Rivestimento", va.ParameterType.Percentage, "Ricostruzioni", "Rivestimento murario", False],
 ]
 
 # RunCommand is the called when the user enters the command name in Rhino.
