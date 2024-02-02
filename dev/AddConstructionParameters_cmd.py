8015a3aed2baf3cf4631c26e82d9ba34cdb7bbe1
andywar65 <andy.war1965@gmail.com>
2024-02-02 17:26:24 +0100

    updated parameter id

--- dev/AddConstructionParameters_cmd.py
+++ dev/AddConstructionParameters_cmd.py
@@ -9,38 +9,41 @@ physical_parameters = [
     ["Unit weight", va.ParameterType.Number, "Physical", "Unit weight of wall (kg/mc)"],
 ]
 wall_demolition_parameters = [
-    ["010-Demolizione", va.ParameterType.Boolean, "Demolizioni", "Demolizione totale muraria", True, "Area"],
-    ["020-Spicconatura", va.ParameterType.Percentage, "Demolizioni", "Spicconatura dell'intonaco murario", False, "Area"],
-    ["030-Raschiatura", va.ParameterType.Percentage, "Demolizioni", "Raschiatura della tinteggiatura muraria", False, "Area"],
-    ["040-Rimozione rivestimento", va.ParameterType.Percentage, "Demolizioni", "Rimozione del rivestimento murario", False, "Area"],
-    ["050-Rimozione battiscopa", va.ParameterType.Percentage, "Demolizioni", "Rimozione del battiscopa", False, "Length"],
+    ["030109-Demolizione", va.ParameterType.Boolean, "Demolizioni", "Demolizione totale muraria", True, "Area"],
+    ["030111-Spicconatura", va.ParameterType.Percentage, "Demolizioni", "Spicconatura dell'intonaco murario", False, "Area"],
+    ["200101-Raschiatura", va.ParameterType.Percentage, "Demolizioni", "Raschiatura della tinteggiatura muraria", False, "Area"],
+    ["030116-Rimozione rivestimento", va.ParameterType.Percentage, "Demolizioni", "Rimozione del rivestimento murario", False, "Area"],
+    ["030116e-Rimozione battiscopa", va.ParameterType.Percentage, "Demolizioni", "Rimozione del battiscopa", False, "Length"],
 ]
 transportation_parameters = [
-    ["010-Scarriolatura", va.ParameterType.Boolean, "Trasporti", "Scarriolatura dei materiali demoliti"],
-    ["020-Tiro", va.ParameterType.Boolean, "Trasporti", "Tiro in alto o calo in basso dei materiali demoliti"],
-    ["030-Trasporto a discarica", va.ParameterType.Boolean, "Trasporti", "Carico e trasporto a discarica dei materiali demoliti"],
-    ["040-Oneri di discarica", va.ParameterType.Boolean, "Trasporti", "Smaltimento in discarica dei materiali demoliti"],
+    ["030303-Scarriolatura", va.ParameterType.Boolean, "Trasporti", "Scarriolatura dei materiali demoliti"],
+    ["030301-Tiro", va.ParameterType.Boolean, "Trasporti", "Tiro in alto o calo in basso dei materiali demoliti"],
+    ["030305-Trasporto a discarica", va.ParameterType.Boolean, "Trasporti", "Carico e trasporto a discarica dei materiali demoliti"],
+    ["030307-Oneri di discarica", va.ParameterType.Boolean, "Trasporti", "Smaltimento in discarica dei materiali demoliti"],
 ]
 wall_construction_parameters = [
-    ["010-Ricostruzione", va.ParameterType.Boolean, "Ricostruzioni", "Costruzioni murarie", True, "Area"],
-    ["020-Intonacatura", va.ParameterType.Percentage, "Ricostruzioni", "Intonacatura muraria", False, "Area"],
-    ["030-Rasatura", va.ParameterType.Percentage, "Ricostruzioni", "Rasatura muraria", False, "Area"],
-    ["040-Imprimitura", va.ParameterType.Percentage, "Ricostruzioni", "Imprimitura muraria con aggrappante", False, "Area"],
-    ["050-Tinteggiatura", va.ParameterType.Percentage, "Ricostruzioni", "Tinteggiatura muraria con lavabile", False, "Area"],
-    ["060-Verniciatura", va.ParameterType.Percentage, "Ricostruzioni", "Verniciatura muraria a smalto", False, "Area"],
-    ["070-Rivestimento", va.ParameterType.Percentage, "Ricostruzioni", "Rivestimento murario", False, "Area"],
-    ["080-Battiscopa", va.ParameterType.Percentage, "Ricostruzioni", "Posa del battiscopa", False, "Length"],
+    ["090206-Ricostruzione", va.ParameterType.Boolean, "Ricostruzioni", "Costruzioni murarie", True, "Area"],
+    ["120104-Intonacatura", va.ParameterType.Percentage, "Ricostruzioni", "Intonacatura muraria", False, "Area"],
+    ["200108-Rasatura", va.ParameterType.Percentage, "Ricostruzioni", "Rasatura muraria", False, "Area"],
+    ["200110-Imprimitura", va.ParameterType.Percentage, "Ricostruzioni", "Imprimitura muraria con aggrappante", False, "Area"],
+    ["200113-Tinteggiatura", va.ParameterType.Percentage, "Ricostruzioni", "Tinteggiatura muraria con lavabile", False, "Area"],
+    ["200114-Verniciatura", va.ParameterType.Percentage, "Ricostruzioni", "Verniciatura muraria a smalto", False, "Area"],
+    ["140201-Rivestimento", va.ParameterType.Percentage, "Ricostruzioni", "Rivestimento murario", False, "Area"],
+    ["140210-Battiscopa", va.ParameterType.Percentage, "Ricostruzioni", "Posa del battiscopa", False, "Length"],
 ]
 slab_demolition_parameters = [
-    ["010-Rimozione pavimento", va.ParameterType.Boolean, "Demolizioni", "Rimozione della pavimentazione", True, "Area"],
-    ["020-Demolizione massetto", va.ParameterType.Boolean, "Demolizioni", "Demolizione del massetto fino a 6 cm", False, "Area"],
+    ["030115-Rimozione pavimento", va.ParameterType.Boolean, "Demolizioni", "Rimozione della pavimentazione", True, "Area"],
+    ["030214-Rimozione parquet", va.ParameterType.Boolean, "Demolizioni", "Rimozione pavimentazione in legno", True, "Area"],
+    ["030114-Demolizione massetto", va.ParameterType.Boolean, "Demolizioni", "Demolizione del massetto fino a 6 cm", False, "Area"],
 ]
 slab_construction_parameters = [
-    ["010-Massetto", va.ParameterType.Boolean, "Ricostruzioni", "Massetto sabbia e cemento", False, "Area"],
-    ["020-Massetto premiscelato", va.ParameterType.Boolean, "Ricostruzioni", "Massetto premiscelato", False, "Area"],
-    ["030-Autolivellante", va.ParameterType.Boolean, "Ricostruzioni", "Autolivellante", False, "Area"],
-    ["040-Pavimentazione", va.ParameterType.Boolean, "Ricostruzioni", "Posa della pavimentazione", True, "Area"],
+    ["070303-Massetto", va.ParameterType.Boolean, "Ricostruzioni", "Massetto sabbia e cemento", False, "Area"],
+    ["070306-Massetto premiscelato", va.ParameterType.Boolean, "Ricostruzioni", "Massetto premiscelato", False, "Area"],
+    ["070308-Autolivellante", va.ParameterType.Boolean, "Ricostruzioni", "Autolivellante", False, "Area"],
+    ["140117-Pavimentazione", va.ParameterType.Boolean, "Ricostruzioni", "Posa della pavimentazione", True, "Area"],
 ]
+finish_demolition_parameters = []
+finish_construction_parameters = []
 
 # RunCommand is the called when the user enters the command name in Rhino.
 # The command name is defined by the filname minus "_cmd.py"
