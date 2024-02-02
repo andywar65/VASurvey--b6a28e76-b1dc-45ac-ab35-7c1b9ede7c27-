9c41e3418449a65c65028fa21887698f030b6584
andywar65 <andy.war1965@gmail.com>
2024-02-02 17:30:19 +0100

    added finish parameters

--- dev/AddConstructionParameters_cmd.py
+++ dev/AddConstructionParameters_cmd.py
@@ -10,8 +10,6 @@ physical_parameters = [
 ]
 wall_demolition_parameters = [
     ["030109-Demolizione", va.ParameterType.Boolean, "Demolizioni", "Demolizione totale muraria", True, "Area"],
-    ["030111-Spicconatura", va.ParameterType.Percentage, "Demolizioni", "Spicconatura dell'intonaco murario", False, "Area"],
-    ["200101-Raschiatura", va.ParameterType.Percentage, "Demolizioni", "Raschiatura della tinteggiatura muraria", False, "Area"],
     ["030116-Rimozione rivestimento", va.ParameterType.Percentage, "Demolizioni", "Rimozione del rivestimento murario", False, "Area"],
     ["030116e-Rimozione battiscopa", va.ParameterType.Percentage, "Demolizioni", "Rimozione del battiscopa", False, "Length"],
 ]
@@ -23,11 +21,6 @@ transportation_parameters = [
 ]
 wall_construction_parameters = [
     ["090206-Ricostruzione", va.ParameterType.Boolean, "Ricostruzioni", "Costruzioni murarie", True, "Area"],
-    ["120104-Intonacatura", va.ParameterType.Percentage, "Ricostruzioni", "Intonacatura muraria", False, "Area"],
-    ["200108-Rasatura", va.ParameterType.Percentage, "Ricostruzioni", "Rasatura muraria", False, "Area"],
-    ["200110-Imprimitura", va.ParameterType.Percentage, "Ricostruzioni", "Imprimitura muraria con aggrappante", False, "Area"],
-    ["200113-Tinteggiatura", va.ParameterType.Percentage, "Ricostruzioni", "Tinteggiatura muraria con lavabile", False, "Area"],
-    ["200114-Verniciatura", va.ParameterType.Percentage, "Ricostruzioni", "Verniciatura muraria a smalto", False, "Area"],
     ["140201-Rivestimento", va.ParameterType.Percentage, "Ricostruzioni", "Rivestimento murario", False, "Area"],
     ["140210-Battiscopa", va.ParameterType.Percentage, "Ricostruzioni", "Posa del battiscopa", False, "Length"],
 ]
@@ -42,8 +35,17 @@ slab_construction_parameters = [
     ["070308-Autolivellante", va.ParameterType.Boolean, "Ricostruzioni", "Autolivellante", False, "Area"],
     ["140117-Pavimentazione", va.ParameterType.Boolean, "Ricostruzioni", "Posa della pavimentazione", True, "Area"],
 ]
-finish_demolition_parameters = []
-finish_construction_parameters = []
+finish_demolition_parameters = [
+    ["030111-Spicconatura", va.ParameterType.Percentage, "Demolizioni", "Spicconatura dell'intonaco murario", False, "Area"],
+    ["200101-Raschiatura", va.ParameterType.Percentage, "Demolizioni", "Raschiatura della tinteggiatura muraria", False, "Area"],
+]
+finish_construction_parameters = [
+    ["120104-Intonacatura", va.ParameterType.Percentage, "Ricostruzioni", "Intonacatura muraria", False, "Area"],
+    ["200108-Rasatura", va.ParameterType.Percentage, "Ricostruzioni", "Rasatura muraria", False, "Area"],
+    ["200110-Imprimitura", va.ParameterType.Percentage, "Ricostruzioni", "Imprimitura muraria con aggrappante", False, "Area"],
+    ["200113-Tinteggiatura", va.ParameterType.Percentage, "Ricostruzioni", "Tinteggiatura muraria con lavabile", False, "Area"],
+    ["200114-Verniciatura", va.ParameterType.Percentage, "Ricostruzioni", "Verniciatura muraria a smalto", False, "Area"],
+]
 
 # RunCommand is the called when the user enters the command name in Rhino.
 # The command name is defined by the filname minus "_cmd.py"
@@ -54,9 +56,13 @@ def RunCommand( is_interactive ):
             va.AddObjectParameter( w_style, p[0], p[1], p[2], p[3])
         for p in wall_demolition_parameters:
             va.AddObjectParameter( w_style, p[0], p[1], p[2], p[3])
+        for p in finish_demolition_parameters:
+            va.AddObjectParameter( w_style, p[0], p[1], p[2], p[3])
         for p in transportation_parameters:
             va.AddObjectParameter( w_style, p[0], p[1], p[2], p[3])
         for p in wall_construction_parameters:
+            va.AddObjectParameter( w_style, p[0], p[1], p[2], p[3])
+        for p in finish_construction_parameters:
             va.AddObjectParameter( w_style, p[0], p[1], p[2], p[3])
     slab_styles = va.GetAllSlabStyleIds()
     for s_style in slab_styles:
@@ -64,10 +70,14 @@ def RunCommand( is_interactive ):
             va.AddObjectParameter( s_style, p[0], p[1], p[2], p[3])
         for p in slab_demolition_parameters:
             va.AddObjectParameter( s_style, p[0], p[1], p[2], p[3])
+        for p in finish_demolition_parameters:
+            va.AddObjectParameter( w_style, p[0], p[1], p[2], p[3])
         for p in transportation_parameters:
             va.AddObjectParameter( s_style, p[0], p[1], p[2], p[3])
         for p in slab_construction_parameters:
             va.AddObjectParameter( s_style, p[0], p[1], p[2], p[3])
+        for p in finish_construction_parameters:
+            va.AddObjectParameter( w_style, p[0], p[1], p[2], p[3])
     return 0
     
 if __name__ == "__main__":
